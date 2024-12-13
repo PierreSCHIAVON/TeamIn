a842e33800cd09d6b1924deabc55003a200323c6
PierreSCHIAVON <98711107+PierreSCHIAVON@users.noreply.github.com>
2024-12-13 23:38:18 +0100

    Add files via upload

---
+++ TagIn/src/main/java/com/example/TagIn/TagInApplication.java
@@ -0,0 +1,13 @@
+package com.example.TagIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagInApplication.class, args);
+	}
+
+}
---
+++ TagIn/src/test/java/com/example/TagIn/TagInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TagIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
